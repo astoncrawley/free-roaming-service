8e6c4c31fb347fd5186d83794e56d3bb2d42eb71
Aston Crawley <astoncrawley@gmail.com>
2025-09-26 19:56:01 +0400

    Add free roaming coverage visualisation

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import plotly.express as px
+import pandas as pd
+
+
+# Import data from GitHub
+data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_with_codes.csv')
+
+print(data.head())
+
+
+# Create basic choropleth map
+fig = px.choropleth(data, locations='iso_alpha', color='gdpPercap', hover_name='country',
+                    projection='natural earth', animation_frame='year',
+                    title='GDP per Capita by Country')
+fig.show()
---
+++ src/providers.py
@@ -0,0 +1,414 @@
+provider_dict = {
+    "A": {
+        "Name": "1pMobile",
+        "Network": "EE",
+        "Freeroaming Countries": [
+            "Austria",
+            "Azores",
+            "Balearic Islands",
+            "Belgium",
+            "Bulgaria",
+            "Canary Islands",
+            "Croatia",
+            "Cyprus (South)",
+            "Czech Republic",
+            "Denmark",
+            "Estonia",
+            "Finland",
+            "France",
+            "French Guiana",
+            "Germany",
+            "Gibraltar",
+            "Greece",
+            "Guadeloupe",
+            "Guernsey",
+            "Hungary",
+            "Iceland",
+            "Ireland (Republic)",
+            "Isle of Man",
+            "Italy",
+            "Jersey",
+            "Latvia",
+            "Liechtenstein",
+            "Lithuania",
+            "Luxembourg",
+            "Madeira",
+            "Malta",
+            "Martinique",
+            "Monaco",
+            "Netherlands",
+            "Norway",
+            "Poland",
+            "Portugal",
+            "Reunion Island",
+            "Romania",
+            "San Marino",
+            "Slovakia",
+            "Slovenia",
+            "Spain",
+            "Sweden",
+            "Switzerland",
+            "Vatican City"
+        ],
+        "Roaming Cap": "14GB (Unlimited according to MSE)"
+    },
+    "B": {
+        "Name": "Asda Mobile",
+        "Network": "Vodafone",
+        "Freeroaming Countries": [
+            "Aland Islands",
+            "Austria",
+            "Azores",
+            "Balearic Islands",
+            "Belgium",
+            "Bulgaria",
+            "Canary Islands",
+            "Croatia",
+            "Cyprus",
+            "Czech Republic",
+            "Denmark",
+            "Estonia",
+            "Finland",
+            "France",
+            "French Guiana",
+            "Germany",
+            "Greece",
+            "Guadeloupe",
+            "Guernsey",
+            "Hungary",
+            "Iceland",
+            "Ireland",
+            "Isle of Man",
+            "Italy",
+            "Jersey",
+            "Latvia",
+            "Liechtenstein",
+            "Lithuania",
+            "Luxembourg",
+            "Madeira",
+            "Malta",
+            "Martinique",
+            "Mayotte",
+            "Netherlands",
+            "Norway",
+            "Poland",
+            "Portugal",
+            "Reunion",
+            "Romania",
+            "Slovakia",
+            "Slovenia",
+            "Spain",
+            "St. Barthelemy",
+            "Sweden",
+            "Vatican City"
+        ],
+        "Roaming Cap": "5GB (Unlimited according to MSE)"
+    },
+    "C": {
+        "Name": "CMLink",
+        "Network": "EE",
+        "Freeroaming Countries": [
+            "??"
+        ],
+    },
+    "D": {
+        "Name": "CTExcel",
+        "Network": "EE"
+    },
+    "E": {
+        "Name": "Ecotalk",
+        "Network": "EE",
+        "Freeroaming Countries": [
+        ],
+        "Roaming Cap": None
+    },
+    "F": {
+        "Name": "EE",
+        "Network": "EE",
+        "Freeroaming Countries": [
+        ],
+        "Roaming Cap": None
+    },
+    "G": {
+        "Name": "giffgaff",
+        "Network": "O2",
+        "Freeroaming Countries": [
+            "Austria",
+            "Belgium",
+            "Bulgaria",
+            "Croatia",
+            "Cyprus (South)",
+            "Czech Republic",
+            "Denmark",
+            "Estonia",
+            "Finland",
+            "France",
+            "French Guiana",
+            "Germany",
+            "Gibraltar",
+            "Greece",
+            "Guadeloupe",
+            "Hungary",
+            "Iceland",
+            "Ireland",
+            "Italy",
+            "Latvia",
+            "Liechtenstein",
+            "Lithuania",
+            "Luxembourg",
+            "Malta",
+            "Martinique",
+            "Mayotte",
+            "Netherlands",
+            "Norway",
+            "Poland",
+            "Portugal",
+            "Reunion",
+            "Romania",
+            "San Marino",
+            "Slovakia",
+            "Slovenia",
+            "Spain",
+            "Sweden",
+            "Ukraine"
+        ],
+        "Roaming Cap": "5GB"
+    },
+    "H": {
+        "Name": "Honest Mobile",
+        "Network": "Three",
+        "Freeroaming Countries": [
+            "Austria",
+            "Belgium",
+            "Bulgaria",
+            "Croatia",
+            "Cyprus (excl North Cyprus)",
+            "Czech Republic",
+            "Denmark",
+            "Estonia",
+            "Finland",
+            "France",
+            "French Guiana",
+            "Germany",
+            "Gibraltar",
+            "Greece",
+            "Guadeloupe",
+            "Guernsey",
+            "Hungary",
+            "Iceland",
+            "Ireland",
+            "Isle of Man",
+            "Italy",
+            "Jersey",
+            "Latvia",
+            "Liechtenstein",
+            "Lithuania",
+            "Luxembourg",
+            "Malta",
+            "Martinique",
+            "Mayotte",
+            "Monaco",
+            "Netherlands",
+            "Norway",
+            "Poland",
+            "Portugal",
+            "Reunion",
+            "Romania",
+            "Saint Barthelemy",
+            "Saint Martin",
+            "San Marino",
+            "Slovakia",
+            "Slovenia",
+            "Spain",
+            "Sweden",
+            "Switzerland"
+        ],
+        "Roaming Cap": "??"
+    },
+    "I": {
+        "Name": "iD Mobile",
+        "Network": "Three",
+        "Freeroaming Countries": [
+            "Austria",
+            "Azerbaijan",
+            "Azores",
+            "Belarus",
+            "Belgium",
+            "Bulgaria",
+            "Canary Islands",
+            "Croatia",
+            "Cyprus (excl North Cyprus)",
+            "Czech Republic",
+            "Denmark",
+            "Estonia",
+            "Finland",
+            "France",
+            "French Guiana",
+            "Germany",
+            "Gibraltar",
+            "Greece",
+            "Guadeloupe",
+            "Guernsey",
+            "Hungary",
+            "Iceland",
+            "Ireland",
+            "Isle of Man",
+            "Italy",
+            "Jersey",
+            "Kazakhstan",
+            "Latvia",
+            "Liechtenstein",
+            "Lithuania",
+            "Luxembourg",
+            "Macedonia",
+            "Madeira",
+            "Malta",
+            "Martinique",
+            "Monaco",
+            "Netherlands",
+            "Norway",
+            "Poland",
+            "Portugal",
+            "Reunion",
+            "Romania",
+            "Saint Barthelemy",
+            "Saint Martin",
+            "San Marino",
+            "Slovakia",
+            "Slovenia",
+            "Spain",
+            "Sweden",
+            "Vatican City"
+        ],
+        "Roaming Cap": "30GB"
+    },
+    "J": {
+        "Name": "Lebara",
+        "Network": "Vodafone",
+        "Freeroaming Countries": [
+            "Austria","Belgium","Bulgaria","Croatia","Cyprus (excl North Cyprus)","Czech Republic","Denmark","Estonia","Finland","France","French Guiana","Germany","Gibraltar","Greece","Guadeloupe","Hungary","Iceland","India","Ireland","Italy","Latvia","Liechtenstein","Lithuania","Luxembourg","Malta","Martinique","Mayotte","Netherlands","Norway","Poland","Portugal","Reunion","Romania","Slovakia","Slovenia","Spain","Sweden"
+        ],
+        "Roaming Cap": "30GB"
+    },
+    "K": {
+        "Name": "Lyca Mobile",
+        "Network": "EE",
+        "Freeroaming Countries": [
+            "??",
+        ],
+        "Roaming Cap": "??"
+    },
+    "L": {
+        "Name": "O2",
+        "Network": "O2",
+        "Freeroaming Countries": [
+            "Austria","Azores","Belgium","Bulgaria","Canary Islands","Croatia","Cyprus (excl North Cyprus)","Czech Republic","Denmark","Estonia","Finland","France","French Guiana","Germany","Gibraltar","Greece","Guadeloupe","Guernsey","Hungary","Iceland","Ireland","Isle of Man","Italy","Jersey","Latvia","Liechtenstein","Lithuania","Luxembourg","Madeira","Malta","Martinique","Mayotte","Monaco","Netherlands","Norway","Poland","Portugal","Reunion","Romania","Saint Barthelemy","Saint Martin","San Marino","Slovakia","Slovenia","Spain","Sweden","Switzerland","Vatican City"
+        ],
+        "Roaming Cap": "25GB?"
+    },
+    "M": {
+        "Name": "Sky Mobile",
+        "Network": "O2",
+        "Freeroaming Countries": [
+        ]
+    },
+    "N": {
+        "Name": "SMARTY",
+        "Network": "Three",
+        "Freeroaming Countries": [
+        ]
+    },
+    "O": {
+        "Name": "spusu",
+        "Network": "EE",
+        "Freeroaming Countries": [
+        ]
+    },
+    "P": {
+        "Name": "Superdrug Mobile",
+        "Network": "Three",
+        "Freeroaming Countries": [
+        ]
+    },
+    "Q": {
+        "Name": "Talkmobile",
+        "Network": "Vodafone",
+        "Freeroaming Countries": [
+            "Austria"
+        ]
+    },
+    "R": {
+        "Name": "Tesco Mobile",
+        "Network": "O2",
+        "Freeroaming Countries": [
+            "Austria"
+        ]
+    },
+    "S": {
+        "Name": "Three",
+        "Network": "Three",
+        "Freeroaming Countries": [
+            "Austria"
+        ]
+    },
+    "T": {
+        "Name": "Your Co-op",
+        "Network": "EE",
+        "Freeroaming Countries": [
+            "Austria"
+        ]
+    },
+    "U": {
+        "Name": "1GLOBAL",
+        "Network": "EE",
+        "Freeroaming Countries": [
+            "Austria"
+        ]
+    },
+    "V": {
+        "Name": "Utility Warehouse",
+        "Network": "EE",
+        "Freeroaming Countries": [
+            "Austria"
+        ]
+    },
+    "W": {
+        "Name": "Vodafone",
+        "Network": "Vodafone",
+        "Freeroaming Countries": [
+            "Austria"
+        ]
+    },
+    "X": {
+        "Name": "VOXI",
+        "Network": "Vodafone",
+        "Freeroaming Countries": [
+            "Austria"
+        ]
+    }
+    }
+
+print(len(provider_dict))
+
+import plotly.express as px
+import country_converter as coco
+
+cc = coco.CountryConverter()
+
+country_list = provider_dict["A"]["Freeroaming Countries"]
+print(country_list)
+
+
+iso_list = cc.convert(names=country_list, to='ISO3')
+print(iso_list)
+
+# iso_alpha = []
+
+# for country in country_list:
+
+# Create basic choropleth map
+fig = px.choropleth(locations=iso_list, hover_name=country_list,
+                    projection='natural earth',
+                    title='GDP per Capita by Country')
+fig.show()
+    
